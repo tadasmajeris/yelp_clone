6910d1a7c2601a18eb143db6b5fb4f49792a4ced
Liam Taylor <liam.taylor888@googlemail.com>
2016-11-14 12:36:20 +0000

    1) Created failing feature test for creation of restaurant entries

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -12,12 +12,25 @@ feature 'restaurants' do
 
 context 'restaurants have been added' do
   before do
-    Restaurant.create(name: 'Dominos')
+    Restaurant.create(name: 'Pizza Express')
   end
     scenario 'display restaurants' do
       visit ('/restaurants')
-      expect(page).to have_content "Dominos"
+      expect(page).to have_content "Pizza Express"
       expect(page).not_to have_content "Restaurant listings: There are currently no listings available"
     end
   end
+
+context 'creating restaurants' do
+  scenario 'prompts a user to fill out a form, then displays the new restaurant' do
+    visit '/restaurants'
+    click_link 'Add a restaurant'
+    fill_in 'name', with: 'Pizza Hut'
+    fill_in 'rating', with: '4'
+    click_button 'Submit'
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content 'Pizza Hut, Rating: 4'
+  end
+end
+
 end
