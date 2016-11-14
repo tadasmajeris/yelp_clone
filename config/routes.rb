ebef0a793b2530af1e42debed2991769beb12d0b
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-11-14 15:57:48 +0000

    added update functionality and failing test for deleting restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get '/restaurants' => 'restaurants#index'
-  get '/restaurants/new' => 'restaurants#new'
-  post '/restaurants' => 'restaurants#create'
-  get '/restaurants/:id' => 'restaurants#show', as: :restaurant
+  resources :restaurants
+
+  # get '/restaurants' => 'restaurants#index'
+  # get '/restaurants/new' => 'restaurants#new'
+  # post '/restaurants' => 'restaurants#create'
+  # get '/restaurants/:id' => 'restaurants#show', as: :restaurant
+  # get 'restaurants/:id/edit' => 'restaurants#edit', as: :edit_destination
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -26,35 +26,46 @@ feature 'restaurants' do
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'Pizza Hut'
-      fill_in 'Rating', with: '4'
       click_button 'Create Restaurant'
       expect(current_path).to eq '/restaurants'
-      expect(page).to have_content 'Pizza Hut Rating: 4'
+      expect(page).to have_content 'Pizza Hut'
     end
   end
 
   context 'viewing restaurants' do
-    let!(:dixy_chicken){ Restaurant.create(name: 'Dixy Chicken', rating: 2) }
+    let!(:dixy_chicken){ Restaurant.create(name: 'Dixy Chicken', description: "Dee fried 'chicken'") }
 
     scenario 'lets a user view a restaurant' do
       visit '/restaurants'
       click_link 'Dixy Chicken'
       expect(current_path).to eq "/restaurants/#{dixy_chicken.id}"
       expect(page).to have_content 'Dixy Chicken'
-      expect(page).to have_content '2'
     end
   end
 
   context 'editing restaurants' do
+    let!(:dixy_chicken){ Restaurant.create(name: 'Dixy Chicken', description: "Dee fried 'chicken'") }
+
     scenario 'let a user edit a restaurant' do
       visit '/restaurants'
       click_link 'Edit Dixy Chicken'
       fill_in 'Name', with: 'Chicken Cottage'
-      fill_in 'Rating', with: 5
-      click_button 'Update restaurant'
+      fill_in 'Description', with: "Deep fried 'chicken'"
+      click_button 'Update Restaurant'
       expect(current_path).to eq '/restaurants'
       expect(page).to have_content 'Chicken Cottage'
-      expect(page).to have_content '5'
+    end
+  end
+
+  context 'deleting restaurants' do
+
+    before { Restaurant.create name: 'Devonshire Chippy', description: 'The best chips in all of Sheffield!' }
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'Devonshire Chippy'
+      expect(page).to have_content 'Restaurant deleted successfully'
     end
   end
 
