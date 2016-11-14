cc3e5559329cfda9170ef10e93c3d000975abeb2
Tadas Majeris <tadas.majeris@gmail.com>
2016-11-14 14:53:12 +0000

    Editing a restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -35,14 +35,31 @@ feature 'restaurants' do
 
   context 'viewing restaurants' do
 
-    let!(:kfc){ Restaurant.create(name:'KFC') }
+    let!(:kfc){ Restaurant.create(name:'KFC', description:'Chicken shop') }
 
     scenario 'lets a user view a restaurant' do
       visit '/restaurants'
       click_link 'KFC'
       expect(page).to have_content 'KFC'
+      expect(page).to have_content 'Chicken shop'
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
   end
-  
+
+  context 'editing restaurants' do
+
+    before { Restaurant.create name:'KFC', description:'Chicken shop' }
+
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      fill_in 'Description', with: 'Deep fried chicken'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Deep fried chicken'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
 end
