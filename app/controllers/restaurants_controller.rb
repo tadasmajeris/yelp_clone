c9965c42711c5ecfd173f28b4a1a7141e34adb6e
glynester <glynester@hotmail.com>
2016-11-14 20:03:49 +0000

    11 - Test and functionality to delete a restaurant.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -27,6 +27,13 @@ class RestaurantsController < ApplicationController
     redirect_to "/restaurants"
   end
 
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice]= "Restaurant successfully deleted"
+    redirect_to '/restaurants'
+  end
+
   private
   def restaurant_params
     list_params_allowed = [:name, :description]
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -21,45 +21,53 @@ feature 'restaurants' do
     end
   end
 
-    context "adding a restaurant" do
-      scenario "prompts user to complete a form and then displays the new restaurant" do
-        visit "/restaurants"
-        click_link("Add a restaurant")
-        fill_in("Name", with: "KFC")
-        # fill_in("rating", with: "3")
-        click_button("Create Restaurant")
-        # expect(page).to have_content("Restaurant successfully added")
-        expect(page).to have_content("KFC")
-        expect(current_path).to eq("/restaurants")
-      end
+  context "adding a restaurant" do
+    scenario "prompts user to complete a form and then displays the new restaurant" do
+      visit "/restaurants"
+      click_link("Add a restaurant")
+      fill_in("Name", with: "KFC")
+      # fill_in("rating", with: "3")
+      click_button("Create Restaurant")
+      # expect(page).to have_content("Restaurant successfully added")
+      expect(page).to have_content("KFC")
+      expect(current_path).to eq("/restaurants")
     end
+  end
 
-    context "viewing restaurants" do
-      let!(:kfc){Restaurant.create(name: "KFC")}
-      scenario "Lets a user view a restaurant" do
-        visit "/restaurants"
-        click_link("KFC")
-        expect(page).to have_content("KFC")
-        expect(current_path).to eq "/restaurants/#{kfc.id}"
-      end
+  context "viewing restaurants" do
+    let!(:kfc){Restaurant.create(name: "KFC")}
+    scenario "Lets a user view a restaurant" do
+      visit "/restaurants"
+      click_link("KFC")
+      expect(page).to have_content("KFC")
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
+  end
 
-    context 'editing restaurants' do
+  context 'editing restaurants' do
 
-      before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
 
-      scenario 'let a user edit a restaurant' do
-       visit '/restaurants'
-       click_link 'Edit KFC'
-       fill_in 'Name', with: 'Kentucky Fried Chicken'
-       fill_in 'Description', with: 'Deep fried goodness'
-       click_button 'Update Restaurant'
-       expect(page).to have_content 'Kentucky Fried Chicken'
-      #  expect(page).to have_content 'Deep fried goodness'
-       expect(current_path).to eq '/restaurants'
-      end
-
+    scenario 'let a user edit a restaurant' do
+     visit '/restaurants'
+     click_link 'Edit KFC'
+     fill_in 'Name', with: 'Kentucky Fried Chicken'
+     fill_in 'Description', with: 'Deep fried goodness'
+     click_button 'Update Restaurant'
+     expect(page).to have_content 'Kentucky Fried Chicken'
+    #  expect(page).to have_content 'Deep fried goodness'
+     expect(current_path).to eq '/restaurants'
     end
+  end
+  context "deleting restaurants" do
+    before {Restaurant.create name:"KFC", description: "Deep fried goodness"}
+    scenario "removes a restaurant when a user clicks a delete link" do
+      visit "/restaurants"
+      click_link "Delete KFC"
+      expect(page).not_to have_content "KFC"
+      expect(page).to have_content "Restaurant successfully deleted"
+    end
+  end
 
 
 end
