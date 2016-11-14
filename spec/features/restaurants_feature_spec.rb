f22c5f5aff1124199fee50421c06f21248f028e3
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-11-14 12:13:45 +0000

    added test for restaurants display

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 feature 'restaurants' do
   context 'no restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Dominos')
+    end
+
     scenario 'should display a prompt to add a restaurant' do
       visit ('/restaurants')
       expect(page).to have_content "Restaurant listings: There are currently no listings available"
-      expect(page).to have_link 'Add a restaurant'
+      expect(page).to have_link "Add a restaurant"
+    end
 
+    scenario 'display restaurants' do
+      visit ('/restaurants')
+      expect(page).to have_content "Dominos"
+      expect(page).not_to have_content "Restaurant listings: There are currently no listings available"
     end
   end
 end
