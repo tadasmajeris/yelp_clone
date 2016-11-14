d601871dcacdb0f182bf51072a96d5d3e7c111e6
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-11-14 14:32:56 +0000

    added failing test for single restaurant view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/restaurants' => 'restaurants#index'
   get '/restaurants/new' => 'restaurants#new'
-  post '/restaurants/' => 'restaurants#create'
+  post '/restaurants' => 'restaurants#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -10,27 +10,39 @@ feature 'restaurants' do
     end
   end
 
-context 'restaurants have been added' do
-  before do
-    Restaurant.create(name: 'Pizza Express')
-  end
-    scenario 'display restaurants' do
-      visit ('/restaurants')
-      expect(page).to have_content "Pizza Express"
-      expect(page).not_to have_content "Restaurant listings: There are currently no listings available"
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Pizza Express')
+    end
+      scenario 'display restaurants' do
+        visit ('/restaurants')
+        expect(page).to have_content "Pizza Express"
+        expect(page).not_to have_content "Restaurant listings: There are currently no listings available"
+      end
+    end
+
+  context 'creating restaurants' do
+    scenario 'prompts a user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Pizza Hut'
+      fill_in 'Rating', with: '4'
+      click_button 'Create Restaurant'
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content 'Pizza Hut, Rating: 4'
     end
   end
 
-context 'creating restaurants' do
-  scenario 'prompts a user to fill out a form, then displays the new restaurant' do
-    visit '/restaurants'
-    click_link 'Add a restaurant'
-    fill_in 'Name', with: 'Pizza Hut'
-    fill_in 'Rating', with: '4'
-    click_button 'Create Restaurant'
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content 'Pizza Hut, Rating: 4'
+  context 'viewing restaurants' do
+    let!(:dixy_chicken){ Restaurant.create(name: 'Dixy Chicken', rating: 2) }
+
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'Dixy Chicken'
+      expect(current_path).to eq "/restaurants/#{dixy_chicken.id}"
+      expect(page).to have_content 'Dixy Chicken'
+      expect(page).to have_content '2'
+    end
   end
-end
 
 end
