92a8003037f2c53d912d5bd2f097f8af667aa7dd
glynester <glynester@hotmail.com>
2016-11-15 10:53:20 +0000

    14 Tests and validation added to model for duplicate name and rating within 1to5.

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -5,4 +5,11 @@ describe "Restaurant", type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it "is not valid unless the name is unique" do
+    Restaurant.create(name: "Chicken Inn")
+    restaurant = Restaurant.new(name: "Chicken Inn")
+    expect(restaurant).to have(1).error_on :name
+  end
+
 end
