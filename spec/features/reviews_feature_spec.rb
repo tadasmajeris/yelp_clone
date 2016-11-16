5ce773db52a973df1ba79dacdad315d355538d73
Liam Taylor <liam.taylor888@googlemail.com>
2016-11-16 15:12:55 +0000

    1) Passed majoirty of tests following addition of associations

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -1,12 +1,13 @@
 feature 'reviewing' do
-  before { Restaurant.create name: 'KFC' }
 
   scenario 'allows users to leave a review using a form' do
-     visit '/restaurants'
-     click_link 'Review KFC'
-     fill_in "Thoughts", with: "so so"
-     select '3', from: 'Rating'
-     click_button 'Leave Review'
+    sign_up
+    add_restaurant
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
 
      expect(current_path).to eq '/restaurants'
      expect(page).to have_content('so so')
